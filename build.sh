9269d874d37ca990e668ef147aa0e0d63c4f07d0
Nil Portugués Calderó <nilopc@gmail.com>
2018-02-06 20:39:01 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+CWD=`$(pwd)`
 
 # Generate release tarball
-cd ./src/
+cd $CWD/src/
 python3 setup.py sdist
 cd ..
 
 # save somewhere the build at: 
-mv ./src/dist/translate_api-0.1.tar.gz ./docker
+mv $CWD/src/dist/translate_api-0.1.tar.gz $CWD/docker
 
